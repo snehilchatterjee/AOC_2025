db5ea815e1bc41a6ce31823f42c00eef54f0b89a
Snehil Chatterjee <snehilchatterjee07@gmail.com>
2025-12-05 17:32:26 +0530

    Day 2 done

---
+++ DAY2_1.cpp
@@ -0,0 +1,156 @@
+typedef long long ll;
+
+#include<iostream>
+#include<bits/stdc++.h>
+#include<cstdio>
+
+using namespace std;
+
+
+/*
+Let's moose it
+*/
+
+void print(vector<ll>& vec){
+    for(auto val: vec){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+}
+
+void print(vector<string>& vec){
+    for(auto val: vec){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+}
+
+void print(vector<vector<ll>>& vec){
+    for(auto val: vec){
+        for(auto v: val){
+            cout<<v<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+}
+
+void print(pair<ll,ll>& p){
+    cout<<"("<<p.first<<","<<p.second<<") "<<endl;
+}
+
+void print(map<ll,ll>& hmap){
+    for(auto val: hmap){
+        cout<<"("<<val.first<<","<<val.second<<") ";
+    }
+    cout<<endl;
+}
+
+void print(vector<pair<ll,ll>>& vec){
+    for(auto val: vec){
+        cout<<"("<<val.first<<","<<val.second<<") ";
+    }
+    cout<<endl;
+}
+
+const int N=1e6+5;     
+const int MOD=1e9+7;   
+
+ll fact[N], inv_fact[N];
+
+ll mod_pow(ll a, ll b, ll mod) {
+    ll res =1;
+    a %= mod;
+    while (b > 0) {
+        if (b & 1) res = res * a % mod;
+        a = a * a % mod;
+        b >>= 1;
+    }
+    return res;
+}
+
+// Precompute factorials and inverse factorials
+void precompute_factorials(int n, int mod) {
+    fact[0] = 1;
+    for (int i = 1; i <= n; ++i)
+        fact[i] = fact[i - 1] * i % mod;
+
+    // Fermat's little theorem: a^(-1) ≡ a^(mod-2) % mod
+    inv_fact[n] = mod_pow(fact[n], mod - 2, mod);
+    for (int i = n - 1; i >= 0; --i)
+        inv_fact[i] = inv_fact[i + 1] * (i + 1) % mod;
+}
+
+ll nCr(int n, int r, int mod) {
+    if (r < 0 || r > n) return 0;
+    return fact[n] * inv_fact[r] % mod * inv_fact[n - r] % mod;
+}
+
+ll floor_log2(ll x){
+    return  (64-__builtin_clzll(x)-1);
+}
+
+vector<string> split(string& str,char c=' '){
+    vector<string> vec;
+    string temp;
+    for(ll i=0;i<str.size();i++){
+        if(str[i]==c){
+            vec.push_back(temp);
+            temp="";
+            continue;
+        }
+        temp.push_back(str[i]);
+    }
+    if(temp.size()) vec.push_back(temp);
+    return vec;
+}
+
+ll getAns(vector<ll>& vec,ll n){
+    ll ans=0;
+    return ans;
+}
+
+
+
+int main() {
+    ios::sync_with_stdio(false); // Fast IO
+    cin.tie(NULL);
+    
+    string str;
+    cin>>str;
+    
+    vector<string> vec=split(str,',');
+    
+    vector<pair<ll,ll>> ranges;
+    
+    for(auto& s: vec){
+        vector<string> tmp=split(s,'-');
+        ranges.push_back({stoll(tmp[0]),stoll(tmp[1])});
+    }
+
+    ll ans=0;
+
+    for(auto& range: ranges){
+        for(ll i=range.first;i<=range.second;i++){
+            string str1=to_string(i);
+            if(str1.size()%2) continue;
+            bool flag=true;
+            ll idx1=0,idx2=str1.size()/2;
+            while(idx2<str1.size()){
+                if(str1[idx1++]!=str1[idx2++]){
+                    flag=false;
+                    break;
+                }
+            }
+
+
+            if(flag) ans+=i;
+        }
+    }
+
+    cout<<ans<<endl;
+
+    return 0;
+}
+
+        
